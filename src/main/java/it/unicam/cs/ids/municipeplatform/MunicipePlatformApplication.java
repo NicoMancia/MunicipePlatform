0d1229368aa87021f90e614b01bb113dac9a61a9
nicola <nicolamancia01@gmail.com>
2024-02-20 18:37:06 +0100

    Initial commit

---
+++ src/main/java/it/unicam/cs/ids/municipeplatform/MunicipePlatformApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.municipeplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunicipePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunicipePlatformApplication.class, args);
+    }
+
+}
