96f0aff8ebbdc677c263b3b79cefe8db4f8990f8
nicola <nicolamancia01@gmail.com>
2024-02-21 14:52:54 +0100

    added Service classes

---
+++ src/main/java/it/unicam/cs/ids/municipeplatform/Search/SearchService.java
@@ -0,0 +1,4 @@
+package it.unicam.cs.ids.municipeplatform.Search;
+
+public interface SearchService {
+}
---
+++ src/main/java/it/unicam/cs/ids/municipeplatform/User/UserService.java
@@ -0,0 +1,4 @@
+package it.unicam.cs.ids.municipeplatform.User;
+
+public interface UserService {
+}
