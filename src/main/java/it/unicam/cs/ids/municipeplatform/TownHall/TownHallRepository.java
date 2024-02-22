aed3d8940928371fbacad605dbd83c8ae1338e79
grellonss <fabio.grelloni@gmail.com>
2024-02-22 17:15:02 +0100

    Implementing ContestServiceImpl.java, NotificationRepository, SearchService, SearchServiceImpl, TownHallRepository

--- src/main/java/it/unicam/cs/ids/municipeplatform/TownHall/TownHallRepository.java
+++ src/main/java/it/unicam/cs/ids/municipeplatform/TownHall/TownHallRepository.java
@@ -2,7 +2,5 @@ package it.unicam.cs.ids.municipeplatform.TownHall;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-
-
 public interface TownHallRepository extends CrudRepository<TownHallEntity,Long> {
 }
