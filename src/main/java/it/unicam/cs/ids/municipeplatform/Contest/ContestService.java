20d1cf8522940da6af3bb36bf367b0e14d6a7f3b
grellonss <fabio.grelloni@gmail.com>
2024-02-22 15:32:11 +0100

    Implementing ContestService

--- src/main/java/it/unicam/cs/ids/municipeplatform/Contest/ContestService.java
+++ src/main/java/it/unicam/cs/ids/municipeplatform/Contest/ContestService.java
@@ -1,4 +1,15 @@
 package it.unicam.cs.ids.municipeplatform.Contest;
 
+
+import java.util.List;
+
 public interface ContestService {
+    ContestEntity createContest(ContestEntity contest, List<Long> contents);
+    ContestEntity getContest(Long id);
+    List<ContestEntity> getAllContest();
+    void updateContest(ContestEntity contest, List<Long> contents);
+
+    void subscribeContent(Long contentId, Long contestId);
+
+    void terminateContest(Long contestId, Long winningContentId);
 }
