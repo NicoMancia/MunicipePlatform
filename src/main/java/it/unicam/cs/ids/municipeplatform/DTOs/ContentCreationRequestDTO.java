80b930fe80cf24d6d6b7c23d4a7a3d39ddd99758
nicola <nicolamancia01@gmail.com>
2024-02-22 10:48:46 +0100

    increase ContentCreationRequestDTO.java

--- src/main/java/it/unicam/cs/ids/municipeplatform/DTOs/ContentCreationRequestDTO.java
+++ src/main/java/it/unicam/cs/ids/municipeplatform/DTOs/ContentCreationRequestDTO.java
@@ -1,4 +1,16 @@
 package it.unicam.cs.ids.municipeplatform.DTOs;
 
+import lombok.*;
+
+import java.util.Date;
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
 public class ContentCreationRequestDTO {
+    private String name;
+    private String description;
+    private Date creationDate;
+    private Long creator;
+    private Long townHall;
 }
