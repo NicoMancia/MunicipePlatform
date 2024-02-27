bb3bd7bbdcccbf17d9d5b403c4844cc914ed7f67
grellonss <fabio.grelloni@gmail.com>
2024-02-27 09:33:30 +0100

    Implementing ContentController, POIController

--- src/main/java/it/unicam/cs/ids/municipeplatform/Content/ContentController.java
+++ src/main/java/it/unicam/cs/ids/municipeplatform/Content/ContentController.java
@@ -1,4 +1,42 @@
 package it.unicam.cs.ids.municipeplatform.Content;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/content")
 public class ContentController {
+    private final ContentService contentService;
+
+    public ContentController(ContentService contentService) {
+        this.contentService = contentService;
+    }
+
+    @PutMapping("/approve/event/{id}")
+    public ResponseEntity<?> approveEvent(@PathVariable Long id, @RequestBody Long userId)
+    {
+        if (contentService.canUserApproveContent(id, userId))
+            return ResponseEntity.badRequest().body("You cannot approve the content.");
+
+        contentService.approveEvent(id);
+        return ResponseEntity.ok().body("{}");
+    }
+    @PutMapping("/approve/poi/{id}")
+    public ResponseEntity<?> approvePoi(@PathVariable Long id, @RequestBody Long userId)
+    {
+        if (contentService.canUserApproveContent(id, userId))
+            return ResponseEntity.badRequest().body("You cannot approve the content.");
+
+        contentService.approvePointOfInterest(id);
+        return ResponseEntity.ok().body("{}");
+    }
+    @PutMapping("/approve/itinerary/{id}")
+    public ResponseEntity<?> approveItinerary(@PathVariable Long id, @RequestBody Long userId)
+    {
+        if (contentService.canUserApproveContent(id, userId))
+            return ResponseEntity.badRequest().body("You cannot approve the content.");
+
+        contentService.approveItinerary(id);
+        return ResponseEntity.ok().body("{}");
+}
 }
--- src/main/java/it/unicam/cs/ids/municipeplatform/POI/POIController.java
+++ src/main/java/it/unicam/cs/ids/municipeplatform/POI/POIController.java
@@ -1,4 +1,50 @@
 package it.unicam.cs.ids.municipeplatform.POI;
 
-public class POIController {
+import it.unicam.cs.ids.municipeplatform.DTOs.POICreationRequestDTO;
+import it.unicam.cs.ids.municipeplatform.Content.ContentService;
+import it.unicam.cs.ids.municipeplatform.BaseCrudController;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/poi")
+public class POIController implements BaseCrudController<POICreationRequestDTO, Long>
+{
+    private final ContentService contentService;
+
+    public POIController(ContentService contentService) {
+        this.contentService = contentService;
+    }
+    @Override
+    public ResponseEntity<POIEntity> create(POICreationRequestDTO dto) {
+        POIEntity newPointOfInterest = contentService.createNewPointOfInterest(new POIEntity(dto));
+        return ResponseEntity.ok(newPointOfInterest);
+    }
+
+    @Override
+    public ResponseEntity<POIEntity> getById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(contentService.getPoi(id));
+    }
+
+    @Override
+    public ResponseEntity<List<POIEntity>> getAll() {
+        return ResponseEntity.ok(contentService.getAllPoi());
+    }
+
+    @Override
+    public ResponseEntity<?> update(POICreationRequestDTO dto, Long id) {
+        POIEntity elem = new POIEntity(dto);
+        elem.setId(id);
+
+        contentService.updatePoi(elem);
+        return ResponseEntity.ok("{}");
+    }
+    @Override
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        return null;
+}
 }
